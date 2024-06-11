bcc4bc8fea4ef30d4b6c507067209987e61eb009
aissiomar <aissiomar@yahoo.fr>
2024-06-11 09:39:22 +0200

    push

---
+++ src/Entity/Book.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $author;
+
+    #[ORM\Column(type: 'integer')]
+    private $publishedYear;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+        return $this;
+    }
+
+    public function getPublishedYear(): ?int
+    {
+        return $this->publishedYear;
+    }
+
+    public function setPublishedYear(int $publishedYear): self
+    {
+        $this->publishedYear = $publishedYear;
+        return $this;
+    }
+}
+
